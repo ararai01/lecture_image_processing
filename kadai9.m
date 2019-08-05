6f4ae009fc26bd86880bdd1d0902efd714565369
ararai01 <50580467+ararai01@users.noreply.github.com>
2019-08-05 23:10:12 +0900

    Update kadai9.m

--- kadai9.m
+++ kadai9.m
@@ -3,7 +3,7 @@
 % 各自，Lenna以外の画像を用いよ．
 % 例
 
-ORG = imread('pet_robot_man.png'); % 画像の読み込み
+ORG = imread('TAN_uwanensyuhikusugi_TP_V.jpg'); % 画像の読み込み
 ORG = rgb2gray(ORG); % 白黒濃淡画像に変換
 imagesc(ORG); colormap(gray); colorbar; % 画像の表示
 pause;
